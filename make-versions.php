1875c958854936dce7ad3bbdf8b22098a8d65625
Mark Kimsal <metrofindings@gmail.com>
2022-04-11 09:46:39 -0400

    feat: templatize the dockerfile

---
+++ make-versions.php
@@ -0,0 +1,25 @@
+<?php
+
+$versions = [
+	'7.3' => [
+		'release' => '7.3.33',
+	],
+	'7.4' => [
+		'release' => '7.4.28',
+	],
+	'8.0' => [
+		'release' => '8.0.17',
+	],
+];
+
+foreach ($versions as $majorVersion => $version) {
+	extract($version);
+	ob_start();
+	include('dockerfile-template');
+	$dockerfile = ob_get_contents();
+	ob_end_clean();
+
+	file_put_contents($majorVersion.'/nginx-fpm/Dockerfile', $dockerfile);
+	echo ( "Wrote $majorVersion/nginx-fpm/Dockerfile ...");
+	echo PHP_EOL;
+}
