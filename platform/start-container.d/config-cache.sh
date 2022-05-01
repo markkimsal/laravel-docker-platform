fb1eb46394879df1992bf37d8157dea721763b2e
Mark Kimsal <metrofindings@gmail.com>
2022-05-01 19:30:59 -0400

    feat: cache config on startup

---
+++ platform/start-container.d/config-cache.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -ex
+
+ENV_FILE="/etc/container_environment.sh"
+ENV_DIR="/etc/container_environment/"
+source $ENV_FILE
+COMMAND="/usr/local/bin/php ./artisan config:cache"
+WORK_DIR=${LARAVEL_HORIZON_WORK_DIR:-"/app"}
+
+cd $WORK_DIR
+if [ ! -f artisan ];
+then
+    echo "No artisan found under $WORK_DIR skipping artisan config:cache ..."
+    exit 0
+fi
+
+chpst -u www-data:www-data -e $ENV_DIR $COMMAND
